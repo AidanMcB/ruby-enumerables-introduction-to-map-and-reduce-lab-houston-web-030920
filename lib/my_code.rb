11414726e75a7011138c5835d0a22c0a43973a59
Aidan K McBride <aidankmcbride@gmail.com>
2020-03-04 19:58:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,8 +22,8 @@ end
 def reduce_to_all_true (array)
 i = 0 
   while i < array.length
-  if array[i] == false 
-    return false 
+  if array[i] != true
+    return false
   else
     return true
   end 
